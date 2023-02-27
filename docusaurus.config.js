4f72e45cdf2f9be96a8874be864168f7242cdf42
Max <max@zptx.dev>
2023-02-27 10:54:30 +0800

    refactor: update edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,7 +46,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            `https://github.com/${organizationName}/${projectName}/tree/master/`,
+            `https://github.com/${organizationName}/${projectName}/edit/master/`,
         },
         blog: false,
         theme: {
